1d7195b63d57a175460ddb61f842ea167a22d5f0
HarshDewang <harshdewang@gmail.com>
2023-10-06 18:42:29 +0530

    Pushing incomplete frontend

---
+++ admindashboardinangular15-main/src/app/pages/users/users.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+import * as d3 from "d3";
+
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent   {
+
+}
